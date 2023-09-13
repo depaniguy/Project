03751390324c21b79886e38125f5ccbbf58a5eac
DeeeMosss <121329109+DeeeMosss@users.noreply.github.com>
2023-09-13 20:55:11 +0500

    project_for_school

---
+++ js.js
@@ -0,0 +1,28 @@
+var btn = document.getElementById("btn");
+var link = document.getElementById("theme");
+
+btn.addEventListener("click", function () { ChangeTheme(); });
+
+function ChangeTheme()
+{
+    let lightTheme = "css.css";
+    let darkTheme = "cssd.css";
+
+    var currTheme = link.getAttribute("href");
+    var theme = "";
+
+    if(currTheme == lightTheme)
+    {
+   	 currTheme = darkTheme;
+   	 theme = "dark";
+    }
+    else
+    {    
+   	 currTheme = lightTheme;
+   	 theme = "light";
+    }
+
+    link.setAttribute("href", currTheme);
+
+    Save(theme);
+}
